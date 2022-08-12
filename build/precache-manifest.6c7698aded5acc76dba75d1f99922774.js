de5f0c3b93c700a98149d59c1552bb018d5a4fdc
roshan <roshnn74@gmail.com>
2022-08-12 09:42:31 +0530

    first commit

---
+++ build/precache-manifest.6c7698aded5acc76dba75d1f99922774.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6094b4e4a9933eb2790f21a531af35a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "142241d3dd56f5ad82a3",
+    "url": "/static/css/main.e8034bf1.chunk.css"
+  },
+  {
+    "revision": "5779978e219c7b147aab",
+    "url": "/static/js/2.d62abf37.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.d62abf37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142241d3dd56f5ad82a3",
+    "url": "/static/js/main.bdbfedac.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.bdbfedac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/static/media/memories-Logo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/static/media/memories-Text.18ebdb6d.png"
+  }
+]);
